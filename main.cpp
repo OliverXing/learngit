0fae206ee80d22aa57d011d21d40b4c49119708c
SinIcarus <xingdonghit@gmail.com>
2017-06-10 17:45:21 +0800

    mathmatic

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a = 0, b = 0, res = 0;
+	char op;
+	std::cout << "请输入整数四则运算式子："<< std::endl;
+	std::cin >> a >> op >> b;
+	res = add(a, b);
+	std::cout << res <<std::endl;
+	std::cin.get();
+	return 0;
+}
