eb39765bc934e068b6c79578684ab02db080835f
WayneX <xingdonghit@gmail.com>
2017-11-06 13:27:01 +0800

    create conflict.cpp

---
+++ conflict.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+
+void sayHello()
+{
+    cout << "hello everybody, welcome to the git's world!";
+}
+
+int main()
+{
+    sayHello();
+    return 0;
+}
